45a91c623df137db3379418ab79f86bbcd329eae
ikor <igor_korenovsk@mail.ru>
2018-08-14 12:22:06 +0300

    обновил аннотацию

--- metrics.php
+++ metrics.php
@@ -1,11 +1,12 @@
 <?php
 /**
  * Plugin Name: Metrics
- * Plugin URI: http://github.com/
+ * Plugin URI: https://github.com/Igor-kor/metrics
  * Description: подключение метрики
- * Version: 0.1
- * Author: Шарангия Игорь
+ * Version: 0.1.1
+ * Author: Игорь Шарангия
  * Author URI: http://vk.com/id117766113
+ * GitHub Plugin URI: https://github.com/Igor-kor/metrics
  */
 
 // Для того, чтобы этот файл не могли подключить вне WordPress
